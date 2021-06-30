84793e002ea8bb4b44f36c65f3af58a9bae53785
Annna <790845853@qq.com>
2021-06-30 18:04:04 +0800

    travis_test

--- task1.sh
+++ task1.sh
@@ -61,7 +61,7 @@ function AddSuffix {
         type=${i##*.}
 	if [[ ${type} != "jpg" && ${type} != "png" && ${type} != "svg" && ${type} != "jpeg" && ${type} != "JPEG" ]]; then continue; fi;
         mv "${i}" "${i%.*}$1"."${type}"
-        echo " ${i} 已添加后缀 ${i%.*}$1"."${type}"
+        echo " ${i} 已添加后缀 ${i%.*}$1.${type}"
     done
 }
 
